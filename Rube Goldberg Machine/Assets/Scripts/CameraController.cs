f3943eec7dc7e9abc8c786a11e9a45c463c4c572
Stella Liu <lius5225@gmail.com>
2016-11-15 13:56:50 -0500

    4 Phases

---
+++ Rube Goldberg Machine/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject[] Cameras;
+	private int currentCam;
+
+	// Use this for initialization
+	void Start () {
+	//	DeactivateCameras();
+	//	Cameras[0].SetActive(true);
+	//	currentCam = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//public void activateCamera(int camNum){
+		//	Cameras[camNum - 1].SetActive(true);
+		//	currentCam = camNum - 1;
+		//}
+
+		//public void DeactivateCameras(){
+		//	for(int i = 0; i < Cameras.Length; i++){
+		//		Cameras[i].SetActive(false);
+		//	}	
+		//}
+	}
+}
---
+++ Rube Goldberg Machine/Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrigger : MonoBehaviour {
+
+	public CameraController controller;
+	public int camNum;
+
+	void OnTriggerEnter(Collider other){
+	//	controller.DeactivateCameras();
+	//	controller.activateCamera(camNum);
+	}
+}
